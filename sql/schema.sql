043c9dd9d0caf06d09c2786a233de59a8378ad66
noah138 <noahschwartz@live.com>
2022-11-01 18:37:26 -0700

    edited schema.sql and filled out seeds.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,19 +6,27 @@ USE employees_db;
 CREATE TABLE department (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
-)
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,3 +1,25 @@
 USE employees_db;
 
-INSERT INTO deparment
+INSERT INTO deparment(name)
+VALUES
+("Marketing"),
+("Finance"),
+("Human Resources"),
+("IT")
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("Sales Lead", 100000, 1)
+("Salesperson", 50000, 2)
+("Lead Engineer", 120000, 3)
+("Accountant", 75000, 4)
+
+
+-- id firstname lastname roleid managerid
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+("John", "Doe", 1, NULL)
+("Jim", "Brown", 2, NULL)
+("Russell", "Wilson", 3, NULL)
+("Geno", "Smith", 4, 1)
+("Kenneth", "Walker", 5, 2)
+("Drew", "Lock", 6, 3)
