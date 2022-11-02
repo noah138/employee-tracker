2709c646c21e0fba3dadc9d6b41005bf54e54b8b
noah138 <noahschwartz@live.com>
2022-11-02 01:46:19 -0700

    Fixed some functionality for the show all and added functionality to add employee

--- server.js
+++ server.js
@@ -63,32 +63,123 @@ function start() {
     })
 }
 
+//  I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewAllEmployees() {
-    db.query('SELECT * FROM employees', function (err, data) {
+    const sql = `SELECT
+                    employees.id,
+                    employees.first_name,
+                    employees.last_name,
+                    roles.title,
+                    departments.name,
+                    roles.salary
+                    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                FROM employees
+                LEFT JOIN roles
+                ON employees.role_id = roles.id
+                LEFT JOIN departments
+                ON roles.department_id = departments.id
+                LEFT JOIN employee AS manager
+                ON employees.manager_id = manager.id
+    `;
+
+    db.query(sql, function (err, data) {
         console.table(data);
         start();
     })
 }
 
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 function addEmployee() {
+    db.query('SELECT * FROM roles', function (err, results) {
+        const roleArr = results.map((role) => {
+            return {
+                name: role.title,
+                value: role.id,
+            }
+        })
 
+    db.query('SELECT * FROM employees', function (err, results) {
+        const employeeArr = results.map((employee) => {
+            return {
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id,
+            }
+        })
+        // Adds the option to select 'none'
+        employeeArr.unshift(
+            {
+                value: null,
+                name: 'None'
+            }
+        )
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: `What is the employee's first name?`,
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: `What is the employee's last name?`,
+            },
+            {
+                type: 'list',
+                name: 'newRole',
+                message: `What is the employee's role?`,
+                choices: roleArr,
+            },
+            {
+                type: 'list',
+                name: 'newManager',
+                message: `Who is the employee's manager?`,
+                choices: employeeArr,
+            },
+        ]).then((response) => {
+            const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+            VALUES (?, ?, (SELECT id FROM roles WHERE title = ?), ?)`
+            
+            db.query(sql, [firstName, lastName, newRole, newManager],function (err, data) {
+                if (err) throw err;
+                console.log(`${firstName} ${lastName} has been added to the database!`);
+                start();
+            })
+        })
+    })
+
+    })
+    
 }
 
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 function updateEmployeeRole() {
 
 }
 
-function deleteEmployee() {
+// function deleteEmployee() {
 
-}
+// }
 
+//  THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewAllRoles() {
-    db.query('SELECT * FROM roles', function (err, data) {
+    sql = `SELECT
+            roles.title,
+            roles.id,
+            departments.name,
+            roles.salary
+        FROM roles
+        LEFT JOIN departments
+        ON roles.department_id = departments.id`
+
+    db.query(sql, function (err, data) {
+        if (err) throw err;
         console.table(data);
         start();
     })
 }
 
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 function addRole() {
     inquirer.prompt([
         {
@@ -101,17 +192,22 @@ function addRole() {
     })
 }
 
-function deleteRole() {
+// function deleteRole() {
 
-}
+// }
 
+// THEN I am presented with a formatted table showing department names and department ids
 function viewAllDeparments() {
-    db.query('SELECT * FROM departments', function (err, data) {
+    const sql = `SELECT * FROM departments`
+
+    db.query(sql, function (err, data) {
+        if (err) throw err;
         console.table(data);
         start();
     })
 }
 
+// THEN I am prompted to enter the name of the department and that department is added to the database
 function addDepartment() {
     inquirer.prompt([
         {
@@ -124,9 +220,9 @@ function addDepartment() {
     })
 }
 
-function deleteDepartment() {
+// function deleteDepartment() {
 
-}
+// }
 
 function quit() {
 
